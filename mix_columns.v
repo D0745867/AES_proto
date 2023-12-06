3e241b3ab1b32f41c70d7a070ba211fa9230a24a
Steve <hungche0514@gmail.com>
2023-12-06 21:53:12 +0000

    Xtime Structure refactor

--- mix_columns.v
+++ mix_columns.v
@@ -1,5 +1,19 @@
 `timescale 1ns/1ns
 
+module xtime (
+    output [7:0] xtime_o;
+    input [7:0] xtime_i;
+);
+    always @(*) begin
+        if(xtime_i [7] == 1'b1) begin
+            xtime_o = (xtime_i << 1) ^ 8'h1b;
+        end
+        else begin
+            xtime_o = xtime_i << 1;
+        end
+    end
+endmodule
+
 module mix_columns (
     output [4*8 - 1 : 0] mix_col_o,
     input [4*8 - 1 : 0] mix_col_in
@@ -13,19 +27,26 @@ module mix_columns (
     assign mix_col_in_2d[1] = mix_col_in[23:16];
     assign mix_col_in_2d[0] = mix_col_in[31:24];
 
-    //Mix One Column
-    function [7:0] xtime;
-    input [7:0] a;
-    begin
-        if (a[7] == 1'b1)  
-        xtime = (a << 1) ^ 8'h1b;
-        else
-        xtime = a << 1;
-    end
-    endfunction
-
+    // //Mix One Column
+    // function [7:0] xtime;
+    // input [7:0] a;
+    // begin
+    //     if (a[7] == 1'b1)  
+    //     xtime = (a << 1) ^ 8'h1b;
+    //     else
+    //     xtime = a << 1;
+    // end
+    // endfunction
+    
     wire [7:0] t = mix_col_in_2d[0] ^ mix_col_in_2d[1] ^ mix_col_in_2d[2] ^ mix_col_in_2d[3];
     wire [7:0] u = mix_col_in_2d[0];
+
+    wire [7:0] x1_in, x2_in, x3_in, x4_in;
+    wire [7:0] x1_out, x2_out, x3_out, x4_out;
+    xtime xtime1();
+    xtime xtime2();
+    xtime xtime3();
+    xtime xtime4();
 
     assign mix_col_o[31:24] = mix_col_in_2d[0] ^ t ^ xtime(mix_col_in_2d[0] ^ mix_col_in_2d[1]); 
     assign mix_col_o[23:16] = mix_col_in_2d[1] ^ t ^ xtime(mix_col_in_2d[1] ^ mix_col_in_2d[2]);
