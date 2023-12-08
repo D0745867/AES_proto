1eb6bcdad6feb8b2c3103441dcd3a1c39dd38926
Steve <hungche0514@gmail.com>
2023-12-08 13:00:28 +0000

    Feat: Mix columns datapath

--- mix_columns.v
+++ mix_columns.v
@@ -1,8 +1,9 @@
 `timescale 1ns/1ns
 
 module xtime (
-    output [7:0] xtime_o;
-    input [7:0] xtime_i;
+    output [7:0] xtime_o,
+    input [7:0] xtime_i,
+    input inv_en
 );
     always @(*) begin
         if(xtime_i [7] == 1'b1) begin
@@ -26,27 +27,34 @@ module mix_columns (
     assign mix_col_in_2d[2] = mix_col_in[15:8];
     assign mix_col_in_2d[1] = mix_col_in[23:16];
     assign mix_col_in_2d[0] = mix_col_in[31:24];
-
-    // //Mix One Column
-    // function [7:0] xtime;
-    // input [7:0] a;
-    // begin
-    //     if (a[7] == 1'b1)  
-    //     xtime = (a << 1) ^ 8'h1b;
-    //     else
-    //     xtime = a << 1;
-    // end
-    // endfunction
     
     wire [7:0] t = mix_col_in_2d[0] ^ mix_col_in_2d[1] ^ mix_col_in_2d[2] ^ mix_col_in_2d[3];
     wire [7:0] u = mix_col_in_2d[0];
 
     wire [7:0] x1_in, x2_in, x3_in, x4_in;
     wire [7:0] x1_out, x2_out, x3_out, x4_out;
-    xtime xtime1();
-    xtime xtime2();
-    xtime xtime3();
-    xtime xtime4();
+
+    always @(*) begin
+        if(inv_en) begin
+            x1_in = ; 
+            x2_in = ;
+            x3_in = ;
+            x4_in = ; 
+        end
+        else begin
+            x1_in = ; 
+            x2_in = ;
+            x3_in = ;
+            x4_in = ;
+        end
+    end
+
+    // Xtime Structrue 
+    xtime xtime1(x1_out, x1_in);
+    xtime xtime2(x2_out, x2_in);
+    xtime xtime3(x3_out, x3_in);
+    xtime xtime4(x4_out, x4_in);
+    
 
     assign mix_col_o[31:24] = mix_col_in_2d[0] ^ t ^ xtime(mix_col_in_2d[0] ^ mix_col_in_2d[1]); 
     assign mix_col_o[23:16] = mix_col_in_2d[1] ^ t ^ xtime(mix_col_in_2d[1] ^ mix_col_in_2d[2]);
